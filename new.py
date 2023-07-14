26b6060b4229ad4a709feffdc529044486f0b596
Surendra Shiwakoti <63626177+shiwakotisurendra@users.noreply.github.com>
2023-07-15 01:03:37 +0200

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
+
+st.set_page_config(layout="wide")
 def main():
     with open('test.html', 'r') as f:
         html_string = f.read()
