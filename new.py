7f9b040b74536f3629db5a140fb6ca8efd97f190
Surendra Shiwakoti <63626177+shiwakotisurendra@users.noreply.github.com>
2023-07-30 00:36:28 +0200

    changed index html

--- new.py
+++ new.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 def main():
-    with open('test.html', 'r') as f:
+    with open('index.html', 'r') as f:
         html_string = f.read()
     components.html(html_string, height=1000,scrolling = True)
 
